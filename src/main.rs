24a82597283b5336653f44955ceb6f743997a345
Damon Swayn <dswayn@deputy.com>
2025-11-27 15:39:54 +1100

    add script mode

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
+use std::env::args;
+use std::io::{Stdin, Write};
+use std::io;
+use std::path::Path;
 use slang::evaluator::{eval, Environment};
 use slang::lexer::Lexer;
 use slang::parser::Parser;
-use std::io;
-use std::io::Write;
 
 fn main() {
     let mut env = Environment::new();
     let stdin = io::stdin();
 
+    let args: Vec<String> = args().collect();
+    if args.len() < 1 {
+        run_repl_mode(&mut env, stdin);
+    } else {
+        run_script_mode(&mut env, &args);
+    }
+}
+
+fn run_script_mode(mut env: &mut Environment, args: &Vec<String>) {
+    let file_path_str = &args[1];
+    let file_path = Path::new(file_path_str);
+    if !file_path.exists() {
+        eprintln!("File not found: {}", file_path_str);
+        return;
+    }
+
+    let file_content = std::fs::read_to_string(file_path).expect("failed to read file");
+    let lexer = Lexer::new(&file_content);
+    let mut parser = Parser::new(lexer);
+    let program = parser.parse_program();
+    println!("{}", eval(&program, &mut env));
+    return
+}
+
+fn run_repl_mode(mut env: &mut Environment, stdin: Stdin) {
     loop {
         print_prompt();
         io::stdout().flush().expect("failed to flush stdout");
---
+++ test_scripts/test_a.sl
@@ -0,0 +1,6 @@
+let a = [1, 2, 3];
+let b = first(a);
+let c = last(a);
+let d = rest(a);
+let e = push(a, 4);
+len(d) + len(e);
